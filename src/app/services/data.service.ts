d1014c884aeb098cb897c4d0b1272db9dd348571
bl0pez <blopez4434@gmail.com>
2023-01-17 23:26:27 -0300

    mostrando posts en el componente

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,12 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts(){
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(tap(posts => {
+        console.log(posts);
+      })
+      );
   }
 
 }
